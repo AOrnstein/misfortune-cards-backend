d37df85cf56bbdc469b63058a6e41b31c49587d0
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-25 11:02:04 -0500

    removes functions that were unneeded

--- db/queries/decks.js
+++ db/queries/decks.js
@@ -1,6 +1,6 @@
 import db from "#db/client";
 
-/** Creates a new deck */
+/** Add a card to a player's deck */
 export async function createDeck({ gameId, userId, cardId }) {
   const sql = `
     INSERT INTO decks
@@ -13,36 +13,4 @@ export async function createDeck({ gameId, userId, cardId }) {
     rows: [deck],
   } = await db.query(sql, [gameId, userId, cardId]);
   return deck;
-}
-
-/** Sends an array of decks */
-export async function getDecks() {
-  const sql = `
-    SELECT *
-    FROM decks
-    `;
-  const { rows: decks } = await db.query(sql);
-  return decks;
-}
-
-/** Sends a specific deck by id */
-export async function getDeckById(id) {
-  const sql = `
-    SELECT *
-    FROM decks
-    WHERE id = $1
-    `;
-  const { rows: deck } = await db.query(sql);
-  return deck;
-}
-
-/** Sends an array of decks by specific user */
-export async function getDecksByUserId(id) {
-  const sql = `
-    SELECT *
-    FROM decks
-    WHERE user_id = $1
-    `;
-  const { rows: decks } = await db.query(sql, [id]);
-  return decks;
 }
