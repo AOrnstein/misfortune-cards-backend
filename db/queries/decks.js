bf13b9e6a4605c19481b4e9871a5c0a48b1ffb66
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-25 07:46:58 -0500

    adds getDeckById to sb queries

--- db/queries/decks.js
+++ db/queries/decks.js
@@ -9,3 +9,14 @@ export async function getDecks() {
   const { rows: decks } = await db.query(sql);
   return decks;
 }
+
+/** Sends a specific deck by id */
+export async function getDeckById(id) {
+  const sql = `
+    SELECT *
+    FROM decks
+    WHERE id = $1
+    `;
+  const { rows: deck } = await db.query(sql);
+  return deck;
+}
