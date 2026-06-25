3c5f4d7b6e016987133aa3b68aea47f93664ee85
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-25 12:04:51 -0400

    add createUser loop for seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+
 import { createUser } from "#db/queries/users";
 
 await db.connect();
@@ -7,5 +9,9 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  await createUser("foo", "bar");
+  for (let i = 0; i < 10; i++) {
+    const randomUsername = faker.internet.username();
+    const randomPassword = faker.internet.password();
+    const user = await createUser(randomUsername, randomPassword);
+  }
 }
