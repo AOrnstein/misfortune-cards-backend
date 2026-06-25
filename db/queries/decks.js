24ec47ad744243e58d226828d790c71055a95dee
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-25 07:51:53 -0500

    adds createDeck to db queries

--- db/queries/decks.js
+++ db/queries/decks.js
@@ -1,5 +1,20 @@
 import db from "#db/client";
 
+/** Creates a new deck */
+export async function createDeck({ gameId, userId, cardId }) {
+  const sql = `
+    INSERT INTO decks
+      (game_id, user_id, card_id)
+    VALUES
+      ($1, $2, $3)
+    RETURNING *
+    `;
+  const {
+    rows: [deck],
+  } = await db.query(sql, [gameId, userId, cardId]);
+  return deck;
+}
+
 /** Sends an array of decks */
 export async function getDecks() {
   const sql = `
