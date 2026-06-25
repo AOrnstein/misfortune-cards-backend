1c4b49d44ccdb8c783fe814254daf63d1bee3e2d
AOrnstein <39391589+AOrnstein@users.noreply.github.com>
2026-06-25 11:30:42 -0600

    Apply suggestion from @AOrnstein

--- db/queries/decks.js
+++ db/queries/decks.js
@@ -1,7 +1,7 @@
 import db from "#db/client";
 
 /** Add card to the player's deck for the game */
-export async function createDeck({ gameId, userId, cardId }) {
+export async function addCardToDeck({ gameId, userId, cardId }) {
   const sql = `
     INSERT INTO decks
       (game_id, user_id, card_id)
