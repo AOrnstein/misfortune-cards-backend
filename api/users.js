7d07af641081725ae5c5a28ef03d7c9efc3da9e8
bcarcamo247 <bcarcamo247@gmail.com>
2026-06-25 12:10:16 -0400

    edit registration/login routes

--- api/users.js
+++ api/users.js
@@ -5,7 +5,10 @@ export default router;
 import { createUser, getUserByUsernameAndPassword } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
-
+/*Route handler used to create an account for user
+requireBody ensures username and password exist
+creates user and token for correct login credentials
+sends 201 successful response */
 router
   .route("/register")
   .post(requireBody(["username", "password"]), async (req, res) => {
@@ -15,7 +18,9 @@ router
     const token = await createToken({ id: user.id });
     res.status(201).send(token);
   });
-
+/*Token is created for user, finds user with correct
+username and password. If username and pw incorrect
+send 401 verification error. */
 router
   .route("/login")
   .post(requireBody(["username", "password"]), async (req, res) => {
