76a43c318622131b750bdc6c8e7844f2993a300b
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-29 11:30:35 -0400

    add comments to routes

--- api/invitations.js
+++ api/invitations.js
@@ -16,6 +16,7 @@ import {
 
 router.use(requireUser);
 
+// Creates an invitation to a game for a specific player. Guards against multiple invitations to the same game.
 router.post("/", requireBody(["gameId", "invitedUserId"]), async (req, res) => {
   const { gameId, invitedUserId } = req.body;
 
@@ -30,16 +31,19 @@ router.post("/", requireBody(["gameId", "invitedUserId"]), async (req, res) => {
   res.status(201).json(invitation);
 });
 
+// Gets a list of player's pending invitations.
 router.get("/pending", async (req, res) => {
   const invitations = await getPendingInvitationsByUserId(req.user.id);
   res.send(invitations);
 });
 
+// Gets a list of DM's sent invitations.
 router.get("/sent", async (req, res) => {
   const invitations = await getSentInvitations(req.user.id);
   res.send(invitations);
 });
 
+// Updates player's invitation status upon accept or decline.
 router.put("/:id", requireBody(["status"]), async (req, res) => {
   const invitation = await updateInvitationStatus(
     req.params.id,
@@ -49,6 +53,7 @@ router.put("/:id", requireBody(["status"]), async (req, res) => {
   res.send(invitation);
 });
 
+// Deletes invitations from the DM's list of sent invitations.
 router.delete("/:id", async (req, res) => {
   const invitation = await deleteInvitation(req.params.id);
   if (!invitation) return res.status(404).send("Invitation not found");
