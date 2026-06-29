1a5ab1790b638bcc2caa5fd344471b2d9a6e98f8
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-29 10:26:53 -0400

    add comments to routes

--- api/cards.js
+++ api/cards.js
@@ -4,10 +4,14 @@ export default router;
 
 import { getCards, getCardById } from "#db/queries/cards";
 
+// Get an array of all cards
+
 router.get("/", async (req, res) => {
   const cards = await getCards();
   res.send(cards);
 });
+
+// Get a specific card by ID
 
 router.param("id", async (req, res, next, id) => {
   const card = await getCardById(id);
