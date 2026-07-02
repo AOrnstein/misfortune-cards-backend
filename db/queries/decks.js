24162627de28340af2bbfe0a3777067a4a387fb3
James Pedigo <ogidep.m.semaj@gmail.com>
2026-07-02 12:14:30 -0500

    KAN-34 Make game deck

--- db/queries/decks.js
+++ db/queries/decks.js
@@ -1,5 +1,22 @@
 import db from "#db/client";
 
+/** Initialize the deck by inserting the entire card list */
+export async function initializeGameDeck({ gameId, dmId, cardId }) {
+  const sql = `
+  INSERT INTO decks
+    (game_id, user_id, card_id)
+  VALUES 
+    ($1, $2, card.id 
+    FROM cards WHERE card.id = $3)
+  RETURNING *
+  `;
+
+  const {
+    rows: [deck],
+  } = await db.query(sql, [gameId, dmId, cardId]);
+  return deck;
+}
+
 /** Add card to the player's deck for the game */
 export async function addCardToDeck({ gameId, userId, cardId }) {
   const sql = `
